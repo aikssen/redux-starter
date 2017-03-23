11222a884e31576fd6eca0fefe9e1b3d1adfd3a9
Ever Cifuentes <ebber@Evers-MacBook-Pro.local>
2017-03-22 23:17:45 -0500

    web sockets implemetation

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
+// system imports
+const fs = require('fs');
+// server imports
 const express = require('express');
+const http = require('http');
+const https = require('https');
+const io = require('socket.io');
+
 const ejs = require('ejs');
 const webpack = require('webpack');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const config = require('./webpack.config.dev');
+// debug imports
 const morgan = require('morgan');
 const winston = require('winston');
-const config = require('./webpack.config.dev');
+// security imports
 // const jwt = require('jsonwebtoken');
 
 const app = express();
 const templates = `${__dirname}/views`;
 const assets = `${__dirname}/dist`;
 const defaultPort = 4001;
+const securePort = 4002;
 const port = process.env.PORT || defaultPort;
 const compiler = webpack(config);
 
-// app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use( bodyParser.json() );
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('X-HTTP-Method-Override'));
@@ -41,10 +50,43 @@ app.get('/*', (req, res, next) => {
 	res.render('index.html');
 });
 
-// app.get('*', (req, res) => {
-// 	res.sendFile( `${__dirname}/views/index.html`);
+// app.listen(port, () => {
+// 	winston.info( `Server Running in port ${port}`);
 // });
 
-app.listen(port, () => {
-	winston.info( `Server Running in port ${port}`);
+const options = {
+  key: fs.readFileSync('ssl/key.pem'),
+  ca: fs.readFileSync('ssl/csr.pem'),
+  cert: fs.readFileSync('ssl/cert.pem')
+};
+
+
+// normal HTTP server
+http.createServer(app).listen(port, () => {
+	winston.info( `Server Running in http://localhost:${port}/`);
 });
+
+// secure server over SSL
+const server = https.createServer(options, app);
+
+server.listen(securePort, () => {
+	winston.info( `Server Running in https://localhost:${securePort}/`);
+});
+
+// const ws = io.connect(`https://localhost:${securePort}/`, { secure: true });
+const ws = io.listen(server);
+// ws.listen(server);
+
+ws.on('connection', (socket) => {
+  winston.info('a user connected');
+
+	socket.on('chat message', (msg) => {
+    ws.emit('chat message', msg);
+  });
+
+	socket.on('disconnect', () => {
+		winston.info('user disconnected');
+	});
+});
+
+
